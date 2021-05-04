77169aa4fe6c525eca2572f35628ff86855dd8c9
amititme68 <amititme68@gmail.com>
2021-05-05 01:08:23 +0530

    Checkpoint1

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-  
-  </React.StrictMode>,
+  <div>
+  </div>,
   document.getElementById('root')
 );
 
