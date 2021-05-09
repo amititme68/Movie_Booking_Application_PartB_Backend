535be17806cd2677c727d012a0209f4415892546
amititme68 <amititme68@gmail.com>
2021-05-09 16:34:42 +0530

    Checkpoint 3:Displaying the Upcoming and Released Movies

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from '../src/common/header/Header.js'
+import Header from './common/header/Header.js'
 import './logo.svg'
-import reportWebVitals from "./reportWebVitals";
+//import reportWebVitals from "./reportWebVitals";
+import Home from './screens/Home/Home'
 
 
-ReactDOM.render(
-  <div>
-    <Header/>
-  </div>,
-  document.getElementById('root')
-);
+//ReactDOM.render(<Header />,document.getElementById('root'));
+ReactDOM.render(<Home />,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
